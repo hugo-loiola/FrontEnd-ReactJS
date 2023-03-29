38f233dcc9f4e3be4c7dfb0686b8be2ed45d2544
Hugo Loiola <hugoloiola.1@gmail.com>
2023-03-29 20:06:35 -0300

    funcionando

--- introducao/pages/index.jsx
+++ introducao/pages/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Cabecalho from "../components/Cabecalho";
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
   return (
-    <div>
+    <div className='container text-center '>
       <Cabecalho />
-      <h1>Hello World</h1>
+      <h1 >Hello World</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 };
