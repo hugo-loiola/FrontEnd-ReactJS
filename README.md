5867eaa81b73fb8c08a9275df943737d3767fc09
Hugo Loiola <hugoloiola.1@gmail.com>
2023-03-28 21:52:24 -0300

    adicoes readme

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 ### Criar um projeto
 
     npx create-next-app@latest nome-projeto
+
+### Iniciar o projeto
+
+    npm run dev
+
+### Base do código
+```jsx
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
+
+```
+* rafce
--- introducao/pages/index.jsx
+++ introducao/pages/index.jsx
@@ -1,13 +1,10 @@
 import React from "react";
+import Cabecalho from "../components/Cabecalho";
 
 const Home = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <h1>Hello World!</h1>
-      <h1>Hello World!</h1>
-      <h1>Hello World!</h1>
-      <h1>Hello World!</h1>
+      <Cabecalho />
     </div>
   );
 };
