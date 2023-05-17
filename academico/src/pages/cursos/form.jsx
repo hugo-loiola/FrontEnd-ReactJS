a0581c604b86edc4d3932fba46833e2373e962d2
Hugo Loiola <hugoloiola.1@gmail.com>
2023-05-16 21:46:43 -0300

    react hook forms

--- academico/src/pages/cursos/form.jsx
+++ academico/src/pages/cursos/form.jsx
@@ -1,24 +1,27 @@
 import Pagina from "@/components/Pagina";
 import React from "react";
 import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const form = () => {
+  const { register, handleSubmit } = useForm();
+
   return (
     <Pagina titulo="Curso">
       <Form>
         <Form.Group className="mb-3" controlId="nome">
           <Form.Label>Nome</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control type="text" {...register("nome")} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="duracao">
           <Form.Label>Duração</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control type="text" {...register("duracao")} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="modalidade">
           <Form.Label>Modalidade</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control type="text" {...register("modalidade")} />
         </Form.Group>
 
         <Button variant="primary" type="submit">
