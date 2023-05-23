702f75a4d052dc7d35fcce661d05709a97619fb7
Hugo Loiola <hugoloiola.1@gmail.com>
2023-05-23 20:27:14 -0300

    cursos malditos

--- academico/src/pages/cursos/form.jsx
+++ academico/src/pages/cursos/form.jsx
@@ -1,56 +1,40 @@
 import Pagina from "@/components/Pagina";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 const form = () => {
+  const { push } = useRouter();
   const { register, handleSubmit } = useForm();
 
   function salvar(dados) {
     const cursos = JSON.parse(window.localStorage.getItem("cursos")) || [];
     cursos.push(dados);
     window.localStorage.setItem("cursos", JSON.stringify(cursos));
-    console.log(dados);
+    push("/cursos");
   }
 
   return (
-    <Pagina titulo="Formulario">
+    <Pagina titulo="Curso">
       <Form>
         <Form.Group className="mb-3" controlId="nome">
-          <Form.Label>Nome:</Form.Label>
+          <Form.Label>Nome: </Form.Label>
           <Form.Control type="text" {...register("nome")} />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="Numero">
-          <Form.Label>Telefone:</Form.Label>
-          <Form.Control type="number" {...register("Numero")} />
+
+        <Form.Group className="mb-3" controlId="duracao">
+          <Form.Label>Duração: </Form.Label>
+          <Form.Control type="text" {...register("duracao")} />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="Email">
-          <Form.Label>Email address:</Form.Label>
-          <Form.Control type="Email" {...register("Email")} />
+
+        <Form.Group className="mb-3" controlId="modalidade">
+          <Form.Label>Modalidade: </Form.Label>
+          <Form.Control type="text" {...register("modalidade")} />
         </Form.Group>
-        <Form.Label>Curso:</Form.Label>
-        <Form.Select {...register("Curso")} controlId="Curso">
-          <option></option>
-          <option>Culinaria</option>
-          <option>Ciencia da Computacao</option>
-          <option>Direito</option>
-          <option>Educao Fisica</option>
-          <option>Medicina</option>
-          <option>Geografia</option>
-        </Form.Select>
-        <br></br>
-        <Form.Label>Modalidade:</Form.Label>
-        <Form.Select {...register("Modadelida")} controlId="Modalidade">
-          <option></option>
-          <option>EAD</option>
-          <option>Presencial</option>
-        </Form.Select>
-        <br></br>
+
         <Button variant="primary" onClick={handleSubmit(salvar)}>
-          salvar
-        </Button>{" "}
-        <Button variant="dark" href="/cursos">
-          Volta
+          Salvar
         </Button>
       </Form>
     </Pagina>
--- academico/src/pages/cursos/index.jsx
+++ academico/src/pages/cursos/index.jsx
@@ -1,49 +1,44 @@
 import Pagina from "@/components/Pagina";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import { Button, FormGroup, Table } from "react-bootstrap";
+import { Table } from "react-bootstrap";
 import { AiOutlineDelete } from "react-icons/ai";
 
 const index = () => {
   const [cursos, setCursos] = useState([]);
+
   useEffect(() => {
     setCursos(JSON.parse(window.localStorage.getItem("cursos")) || []);
   }, []);
-  console.log(cursos);
+
   return (
     <Pagina titulo="Cursos">
-      <Table striped bordered hover variant="dark">
+      <Link href={"/cursos/form"} className="btn btn-primary mb-2">
+        Novo
+      </Link>
+
+      <Table striped bordered hover>
         <thead>
           <tr>
             <th>#</th>
-            <th>Del</th>
-            <th>NOME</th>
-            <th>Telefone</th>
-            <th>Email</th>
-            <th>Curso</th>
+            <th>Nome</th>
+            <th>Duração</th>
             <th>Modalidade</th>
           </tr>
         </thead>
         <tbody>
           {cursos.map((item, i) => (
             <tr>
-              <td>{i}</td>
               <td>
                 <AiOutlineDelete className="text-danger" />
               </td>
               <td>{item.nome}</td>
-              <td>{item.Numero}</td>
-              <td>{item.Email}</td>
-              <td>{item.Curso}</td>
-              <td>{item.Modadelida}</td>
+              <td>{item.duracao}</td>
+              <td>{item.modalidade}</td>
             </tr>
           ))}
         </tbody>
       </Table>
-      <br></br>
-      <Button href="/cursos/form" variant="primary">
-        Inscreva-se
-      </Button>{" "}
     </Pagina>
   );
 };
