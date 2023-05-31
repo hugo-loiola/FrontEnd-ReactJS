d3e2cd7ee3dfbfd14f248c66cb4e4d9fe0d726b2
Hugo Loiola <hugoloiola.1@gmail.com>
2023-05-31 20:49:24 -0300

    boa

--- academico/pages/cursos/index.js
+++ academico/pages/cursos/index.js
@@ -1,36 +1,35 @@
-import Pagina from '@/components/Pagina'
-import apiFilmes from '@/services/apiFilmes'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
-import { Button, Table } from 'react-bootstrap'
-import { AiOutlineDelete } from 'react-icons/ai'
-import { BsFillPencilFill } from 'react-icons/bs'
+import Pagina from "@/components/Pagina";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { Button, Table } from "react-bootstrap";
+import { AiOutlineDelete } from "react-icons/ai";
+import { BsFillPencilFill } from "react-icons/bs";
 
 const index = () => {
-
-  const [cursos, setCursos] = useState([])
+  const [cursos, setCursos] = useState([]);
 
   useEffect(() => {
-    setCursos(getAll())
-  }, [])
+    setCursos(getAll());
+  }, []);
 
   function getAll() {
-    return JSON.parse(window.localStorage.getItem('cursos')) || []
+    return JSON.parse(window.localStorage.getItem("cursos")) || [];
   }
 
   function excluir(id) {
-    if (confirm('Deseja realmente excluir o registro?')) {
-      const cursos = getAll()
-      cursos.splice(id, 1)
-      window.localStorage.setItem('cursos', JSON.stringify(cursos))
-      setCursos(cursos)
+    if (confirm("Deseja realmente excluir o registro?")) {
+      const cursos = getAll();
+      cursos.splice(id, 1);
+      window.localStorage.setItem("cursos", JSON.stringify(cursos));
+      setCursos(cursos);
     }
   }
 
   return (
     <Pagina titulo="Cursos">
-
-      <Link href={'/cursos/form'} className="btn btn-primary mb-2" >Novo</Link>
+      <Link href={"/cursos/form"} className="btn btn-primary mb-2">
+        Novo
+      </Link>
 
       <Table striped bordered hover>
         <thead>
@@ -45,10 +44,13 @@ const index = () => {
           {cursos.map((item, i) => (
             <tr key={i}>
               <td>
-                <Link href={'/cursos/' + i}>
-                  <BsFillPencilFill className='me-2 text-primary' />
+                <Link href={"/cursos/" + i}>
+                  <BsFillPencilFill className="me-2 text-primary" />
                 </Link>
-                <AiOutlineDelete onClick={() => excluir(i)} className='text-danger' />
+                <AiOutlineDelete
+                  onClick={() => excluir(i)}
+                  className="text-danger"
+                />
               </td>
               <td>{item.nome}</td>
               <td>{item.duracao}</td>
@@ -58,8 +60,7 @@ const index = () => {
         </tbody>
       </Table>
     </Pagina>
-  )
-}
+  );
+};
 
-export default index
-
+export default index;
