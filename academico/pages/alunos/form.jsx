7533b067df9fbd761b74d07f7caaab36dc0014b3
Hugo Loiola <hugoloiola.1@gmail.com>
2023-06-01 21:37:51 -0300

    alunos

---
+++ academico/pages/alunos/form.jsx
@@ -0,0 +1,93 @@
+import Pagina from "@/components/Pagina";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { AiOutlineCheck } from "react-icons/ai";
+import { IoMdArrowRoundBack } from "react-icons/io";
+
+const form = () => {
+  const { push } = useRouter();
+  const { register, handleSubmit } = useForm();
+
+  function salvar(dados) {
+    axios.post("/api/alunos", dados);
+    push("/alunos");
+  }
+
+  return (
+    <Pagina titulo="Aluno">
+      <Form>
+        <Form.Group className="mb-3" controlId="nome">
+          <Form.Label>Nome: </Form.Label>
+          <Form.Control type="text" {...register("nome")} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="cpf">
+          <Form.Label>CPF: </Form.Label>
+          <Form.Control type="text" {...register("cpf")} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="matricula">
+          <Form.Label>Matricula: </Form.Label>
+          <Form.Control type="text" {...register("matricula")} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="salario">
+          <Form.Label>Salario: </Form.Label>
+          <Form.Control type="text" {...register("salario")} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email: </Form.Label>
+          <Form.Control type="text" {...register("email")} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="telefone">
+          <Form.Label>Telefone: </Form.Label>
+          <Form.Control type="text" {...register("telefone")} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="cep">
+          <Form.Label>CEP: </Form.Label>
+          <Form.Control type="text" {...register("cep")} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="logradouro">
+          <Form.Label>Logradouro: </Form.Label>
+          <Form.Control type="text" {...register("logradouro")} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="complemento">
+          <Form.Label>Complemento: </Form.Label>
+          <Form.Control type="text" {...register("complemento")} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="numero">
+          <Form.Label>Numero: </Form.Label>
+          <Form.Control type="text" {...register("numero")} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="bairro">
+          <Form.Label>Bairro: </Form.Label>
+          <Form.Control type="text" {...register("bairro")} />
+        </Form.Group>
+
+        <div className="text-center">
+          <Button variant="success" onClick={handleSubmit(salvar)}>
+            <AiOutlineCheck className="me-1" />
+            Salvar
+          </Button>
+          <Link href={"/alunos"} className="ms-2 btn btn-danger">
+            <IoMdArrowRoundBack className="me-1" />
+            Voltar
+          </Link>
+        </div>
+      </Form>
+    </Pagina>
+  );
+};
+
+export default form;
--- academico/pages/alunos/index.jsx
+++ academico/pages/alunos/index.jsx
@@ -1,8 +1,81 @@
 import Pagina from "@/components/Pagina";
-import React from "react";
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { Button, Table } from "react-bootstrap";
+import { AiOutlineDelete } from "react-icons/ai";
+import { BsFillPencilFill } from "react-icons/bs";
 
 const index = () => {
-  return <Pagina>index</Pagina>;
+  const [alunos, setalunos] = useState([]);
+
+  useEffect(() => {
+    getAll();
+  }, []);
+
+  function getAll() {
+    axios.get("/api/alunos").then((res) => {
+      setalunos(res.data);
+    });
+  }
+
+  function excluir(id) {
+    if (confirm("Are you sure you want to ?")) {
+      axios.delete(`/api/alunos/${id}`);
+      getAll();
+    }
+  }
+
+  return (
+    <Pagina titulo="Alunos">
+      <Link href={"/alunos/form"} className="btn btn-primary mb-2">
+        Novo
+      </Link>
+
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>CPF</th>
+            <th>Matricula</th>
+            <th>Email</th>
+            <th>Telefone</th>
+            <th>CEP</th>
+            <th>Logradouro</th>
+            <th>Complemento</th>
+            <th>Numero</th>
+            <th>Bairro</th>
+          </tr>
+        </thead>
+        <tbody>
+          {alunos.map((item) => (
+            <tr key={item.id}>
+              <td>
+                <Link href={`/alunos/${item.id}`}>
+                  <BsFillPencilFill className="me-2 text-primary" />
+                </Link>
+                <AiOutlineDelete
+                  onClick={() => excluir(item.id)}
+                  className="text-danger"
+                />
+              </td>
+              <td>{item.nome}</td>
+              <td>{item.cpf}</td>
+              <td>{item.matricula}</td>
+              <td>{item.email}</td>
+              <td>{item.telefone}</td>
+              <td>{item.cep}</td>
+              <td>{item.logradouro}</td>
+              <td>{item.complemento}</td>
+              <td>{item.numero}</td>
+              <td>{item.bairro}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Pagina>
+  );
 };
 
 export default index;
