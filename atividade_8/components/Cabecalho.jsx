03c673a35dc36a805d0e4787933f67b2a7c6575a
Hugo Loiola <hugoloiola.1@gmail.com>
2023-06-04 22:15:50 -0300

    correçoes

--- atividade_8/components/Cabecalho.jsx
+++ atividade_8/components/Cabecalho.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 const Cabecalho = () => {
-    return (
-        <>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="/cursos">Acadêmico</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="/cursos">Cursos</Nav.Link>
-                        <Nav.Link href="/disciplinas">Disciplinas</Nav.Link>
-                        <Nav.Link href="/alunos">Alunos</Nav.Link>
-                        <Nav.Link href="/professores">Professores</Nav.Link>
-                        <Nav.Link href="/salas">Salas</Nav.Link>
-                        <Nav.Link href="/semestres">Semestres</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="/cursos">Acadêmico</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/cursos">Cursos</Nav.Link>
+            <Nav.Link href="/disciplinas">Disciplinas</Nav.Link>
+            <Nav.Link href="/alunos">Alunos</Nav.Link>
+            <Nav.Link href="/professores">Professores</Nav.Link>
+            <Nav.Link href="/salas">Salas</Nav.Link>
+            <Nav.Link href="/semestres">Semestres</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
-export default Cabecalho
+export default Cabecalho;
